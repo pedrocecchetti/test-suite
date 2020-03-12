2308095d815301febb3c46fab66670abb158b773
Cecchetti <pedro.cecchetti@gmail.com>
2020-03-12 20:57:50 +0100

    feat: Created model to save all the info into the Database

---
+++ model.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime, Text
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class TestRun(Base):
+    __tablename__ = 'test_run'
+
+    id = Column(Integer, primary_key=True)
+    test = Column(String)
+    created_at = Column(DateTime)
+    started_at = Column(DateTime)
+    finished_at = Column(DateTime)
+    status = Column(Text)
