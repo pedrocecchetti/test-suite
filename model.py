c2d91102dd3900f72b3812ea0991183a13d1f3f8
Cecchetti <pedro.cecchetti@gmail.com>
2020-03-12 22:32:06 +0100

    feat: Created logs, and enviroment columns

--- model.py
+++ model.py
@@ -7,8 +7,10 @@ class TestRun(Base):
     __tablename__ = 'test_run'
 
     id = Column(Integer, primary_key=True)
+    enviroment = Column(String)
     test = Column(String)
     created_at = Column(DateTime)
     started_at = Column(DateTime)
     finished_at = Column(DateTime)
     status = Column(Text)
+    logs = Column(Text)
