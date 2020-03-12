48df87926b88ce2a3542d88ee2e1b374dd7ff43d
Cecchetti <pedro.cecchetti@gmail.com>
2020-03-12 22:02:02 +0100

    feat: Added Argparse to make it better app

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+#! usr/bin/env python
 import datetime
+import argparse
 import subprocess
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -6,6 +8,8 @@ from sqlalchemy.orm import sessionmaker
 from model import TestRun, Base
 
 # Configuring Database
+
+
 def create_session():
     engine = create_engine('postgresql://postgres:postgres@localhost:5432')
     Session = sessionmaker(bind=engine)
@@ -15,9 +19,11 @@ def create_session():
     Base.metadata.create_all(engine)
     return session
 
+
 def execute_subprocess():
     started_at = datetime.datetime.now()
     return started_at, subprocess.run(["pytest -rA -v &"], shell=True, text=True, capture_output=True)
+
 
 def run_all_tests():
     created_at = datetime.datetime.now()
@@ -32,19 +38,59 @@ def treat_results(results):
     resume = results[-2].replace("=", '')
     return end_time, summary, resume
 
-if __name__ == '__main__':
+
+def run(args):
     session = create_session()
-    created_at, started_at, results = run_all_tests()
+    if args.run:
+        created_at, started_at, results = run_all_tests()
+        end_time, summary, resume = treat_results(results.stdout.split('\n'))
 
-    end_time, summary, resume = treat_results(results.stdout.split('\n'))
+        test_run = TestRun(
+            test=f"Test {created_at.timestamp()}",
+            created_at=created_at,
+            started_at=started_at,
+            finished_at=end_time,
+            status=resume
+        )
 
-    test_run = TestRun(
-        test="Teste 1",
-        created_at=created_at,
-        started_at=datetime.datetime.now(),
-        finished_at=end_time,
-        status=resume
+        session.add(test_run)
+        session.commit()
+    elif args.query:
+        queryset = session.query(TestRun).all()
+        list_of_tests = []
+        for item in queryset:
+            item_dict = {
+                'name': item.test,
+                'created_at': str(item.created_at),
+                'started_at': str(item.started_at),
+                'finished_at': str(item.finished_at),
+                'status': item.status
+            }
+            list_of_tests.append(item_dict)
+
+        print(list_of_tests)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Run tests from a test suite and save the information from the tests on a Database"
     )
+    parser.add_argument(
+        '--run',
+        help='This option is used to run all the tests and store the info in the DB',
+        action='store_const',
+        const=True
+    )
+    parser.add_argument(
+        '--query',
+        help='This option is used to get the tests info from the DB',
+        action='store_const',
+        const=True
+    )
+    parser.set_defaults(func=run)
+    args = parser.parse_args()
+    args.func(args)
 
-    session.add(test_run)
-    session.commit()
+
+if __name__ == '__main__':
+    main()
